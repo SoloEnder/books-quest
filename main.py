3398476fa0b50e2ff203f852ff761df05d912da2
SoloEnder <houngbedjileonide@protonmail.com>
2025-05-26 19:20:38 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+import sys, json
+sys.path.append("program")
+import users_manager
+import home
+
+w_c = "white"
+# Fonctions
+def add_user():
+	users_manager.add_user(home_w)
+
+# Fenêtre
+
+home_w.mainloop()
