3e64f31dad2e3f50c134db98ea866d54fa12bdd6
SoloEnder <houngbedjileonide@protonmail.com>
2026-01-13 21:03:57 +0100

    added logging stuff

--- app/app.py
+++ app/app.py
@@ -1,10 +1,16 @@
 
+import logging
+
 from app.ui import ui
 
 class App:
 
     def __init__(self):
+        self.logger = logging.getLogger(__name__)
+        self.logger.info("Initialising application...")
         self.ui = ui.UI(self)
+
         
     def running(self):
+        self.logger.info("Showing main window...")
         self.ui.mainloop()
