a31177276ad15944e8fc9478054787ea21564c31
SoloEnder <houngbedjileonide@protonmail.com>
2026-01-16 18:41:12 +0100

    my_rotating_file_handler.py is now my_logging stuff.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 
 from app.utils import paths
-from app.src import my_rotating_logsfile_handler
+from app.src import my_logging_stuff
 import logging
 
 logger = logging.getLogger("app")
@@ -9,9 +9,11 @@ log_msg_formatter = logging.Formatter(fmt="[{asctime}] - [{name}/{levelname}] : 
 logs_streamhandler = logging.StreamHandler()
 logs_streamhandler.setFormatter(log_msg_formatter)
 logs_streamhandler.setLevel(logging.DEBUG)
-logs_filehandler = my_rotating_logsfile_handler.MyRotatingFileHandler(paths.get_absfilepath("logs", "app.log"))
+logs_filehandler = my_logging_stuff.MyRotatingFileHandler(paths.get_absfilepath("logs", "app.log"))
 logs_filehandler.setFormatter(log_msg_formatter)
 logs_filehandler.setLevel(logging.INFO)
 logger.addHandler(logs_streamhandler)
 logger.addHandler(logs_filehandler)
 logger.setLevel(logging.DEBUG)
+logger.addFilter(my_logging_stuff.ErrorsFilter())
+logger.addFilter(my_logging_stuff.SensitiveInfoFilter())
---
+++ app/src/my_logging_stuff.py
@@ -0,0 +1,35 @@
+
+import os
+import pathlib
+import logging.handlers
+from tkinter.messagebox import showerror
+
+class MyRotatingFileHandler(logging.handlers.RotatingFileHandler):
+
+    def __init__(self, filename, mode='a', maxBytes=0, backupCount=0,
+                 encoding=None, delay=False, errors=None):
+        file_parent = pathlib.Path(filename).parent
+
+        if not file_parent.exists():
+            file_parent.mkdir(parents=True)
+
+        super().__init__(filename, mode, maxBytes, backupCount, encoding=None, delay=False, errors=None)
+
+class SensitiveInfoFilter(logging.Filter):
+
+    def filter(self, record):
+        userpath = os.path.expanduser("~")
+
+        if userpath.lower() in record.getMessage().lower():
+            record.msg = record.getMessage().replace(userpath, "~")
+            record.args = ()
+        return True
+    
+class ErrorsFilter(logging.Filter):
+
+    def filter(self, record):
+
+        if record.levelname == "ERROR" or record.levelname == "CRITICAL":
+            showerror(title="Error", message=record.getMessage())
+
+        return True
