df247e59ff0dae6778812f18d4df95a0c50da780
SoloEnder <houngbedjileonide@protonmail.com>
2026-01-16 18:42:20 +0100

    added book handler

--- app/app.py
+++ app/app.py
@@ -2,13 +2,15 @@
 import logging
 
 from app.ui import ui
+from app.src import book
 
 class App:
 
     def __init__(self):
         self.logger = logging.getLogger(__name__)
         self.logger.info("Initialising application...")
-        self.ui = ui.UI(self)
+        self.books_handler = book.BooksHandler()
+        self.ui = ui.UI(self, self.books_handler)
 
         
     def running(self):
