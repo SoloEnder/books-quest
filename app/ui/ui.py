f4ce625c738fceb1f926abeddb4a8b306d88bdbb
SoloEnder <houngbedjileonide@protonmail.com>
2026-01-22 21:36:38 +0100

    - added events handler

--- app/ui/ui.py
+++ app/ui/ui.py
@@ -8,18 +8,20 @@ from app.utils import paths
 
 class UI(ctk.CTk):
 
-    def __init__(self, app, books_handler, **kwargs):
+    def __init__(self, app, books_handler, events_handler, **kwargs):
         super().__init__(**kwargs)
         self.logger = logging.getLogger(__name__)
         self.logger.info("Initialising user interface...")
         self.app = app
         self.books_handler = books_handler
+        self.events_handler = events_handler
         self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
         self.columnconfigure(1, weight=4)
         ctk.FontManager.load_font(str(paths.get_abspath("app/assets/icons.ttf")))
-        self.menu_bar = menu_bar.MenuBar(self)
-        self.menu_bar.add_tab(label="Books", tab_object=books_page.BooksTab(self, self.books_handler))
+        self.books_tab = books_page.BooksTab(self, self.books_handler, self.events_handler)
+        self.menu_bar = menu_bar.MenuBar(self, self.events_handler)
+        self.menu_bar.add_tab(label="Books", tab_object=self.books_tab)
         self.menu_bar.draw_widgets()
         self.menu_bar.switch_tab("Books")
         self.menu_bar.draw_tab()
