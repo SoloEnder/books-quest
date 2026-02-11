c123de5d28fff1654903113b552b6c7de83f641e
SoloEnder <houngbedjileonide@protonmail.com>
2026-02-11 21:29:34 +0100

    deleted useless buttons widgets, corrected texts provided by labels

--- app/ui/pages/books_page.py
+++ app/ui/pages/books_page.py
@@ -21,6 +21,8 @@ class BooksPage(QtWidgets.QWidget):
         self.scroll_area = QtWidgets.QScrollArea(self)
         self.scroll_area.setWidgetResizable(True)
         self.scroll_area.setWidget(self.shelfs_container)
+        
+        #Shelf books view
         self.main_layout.addWidget(self.add_book_b, 0, 0, QtCore.Qt.AlignmentFlag.AlignLeft)
         self.main_layout.addWidget(self.add_shelf_b, 1, 0, QtCore.Qt.AlignmentFlag.AlignLeft)
         self.main_layout.addWidget(self.scroll_area, 2, 0)
@@ -59,6 +61,7 @@ class ShelfWidget(QtWidgets.QWidget):
         super().__init__()
         self.icons_folder = paths.get_abspath("app/assets/icons")
         self.shelf = shelf
+        #self.
         self.main_layout = QtWidgets.QGridLayout(self)
         self.cover_pm = QtGui.QPixmap("app/assets/default_shelf_cover.png")
         self.cover_lb = QtWidgets.QLabel()
@@ -68,41 +71,70 @@ class ShelfWidget(QtWidgets.QWidget):
             font-weight: bold;
             font-size: 20px;
         """)
-        self.total_books = QtWidgets.QLabel(f"{len(self.shelf.books)} books")
+        self.total_books = QtWidgets.QLabel(f"{len(self.shelf.books)} livres")
         self.total_books.setStyleSheet("""
             font-size: 17px;
         """)
+        self.unread_books_count = 0
+        self.on_reading_books_count = 0
+        self.finished_books_count = 0
+        
+        
+        for book in self.shelf.books.values():
+
+            if book.status == "unread":
+                self.unread_books_count += 1
+
+            elif book.status == "on_reading":
+                self.on_reading_books_count += 1
+
+            elif book.status == "finished":
+                self.finished_books_count += 1
+
+        self.unread_books_lb = QtWidgets.QLabel(f"{self.unread_books_count} non lus")
+        self.on_reading_books_lb = QtWidgets.QLabel(f"{self.on_reading_books_count} en cours de lecture")
+        self.finished_books_lb = QtWidgets.QLabel(f"{self.finished_books_count} terminés")
         self.total_pages = QtWidgets.QLabel("600 pages")
         self.total_pages.setStyleSheet("""
             font-style: italic;
             font-size: 16px;
         """)
+        self.unread_books_lb.setIndent(10)
+        self.on_reading_books_lb.setIndent(10)
+        self.finished_books_lb.setIndent(10)
+
         self.button_size = QtWidgets.QSizePolicy()
-        self.add_book_b = QtWidgets.QPushButton("Ajouter un livre")
-        self.add_book_b.setSizePolicy(self.button_size)
-        self.add_book_b.setIcon(QtGui.QIcon(str(os.path.join(self.icons_folder, "add_ico"))))
-        self.edit_b = QtWidgets.QPushButton("Editer")
-        self.edit_b.setIcon(QtGui.QIcon(os.path.join(self.icons_folder, "edit_ico.png")))
-        self.edit_b.setSizePolicy(self.button_size)
         self.view_b = QtWidgets.QPushButton("Voir les livres")
         self.view_b.setIcon(QtGui.QIcon(os.path.join(self.icons_folder, "view_books_ico.png")))
         self.view_b.setSizePolicy(self.button_size)
+
         self.main_layout.setAlignment(QtCore.Qt.AlignmentFlag.AlignLeft)
-        self.main_layout.addWidget(self.cover_lb, 0, 0, 6, 1)
-        self.main_layout.addWidget(self.name_lb, 0, 1)
+        self.main_layout.addWidget(self.name_lb, 0, 0)
+        self.main_layout.addWidget(self.cover_lb, 1, 0, 6, 1)
         self.main_layout.addWidget(self.total_books, 1, 1)
-        self.main_layout.addWidget(self.total_pages, 2, 1)
-        self.main_layout.addWidget(self.add_book_b, 3, 1, 1, 1)
-        self.main_layout.addWidget(self.edit_b, 4, 1, 1, 1)
-        self.main_layout.addWidget(self.view_b, 5, 1, 1, 1)
+        self.main_layout.addWidget(self.unread_books_lb, 2, 1)
+        self.main_layout.addWidget(self.on_reading_books_lb, 3, 1)
+        self.main_layout.addWidget(self.finished_books_lb, 4, 1)
+        self.main_layout.addWidget(self.view_b, 5, 1)
 
 class BookWidget(QtWidgets.QWidget):
 
-    def __init__(self):
+    def __init__(self, book):
         super().__init__()
-        self.grid_layout = QtWidgets.QGridLayout(self)
-        self.book_title_lb = QtWidgets.QLabel("Le Manoir de CastleCatz")
-        self.book_description_tb = QtWidgets.QTextEdit("Ceci est un résumé !")
-        self.book_description_tb.setReadOnly(True)
-        self.grid_layout.addWidget(self.book_title_lb)
-        self.grid_layout.addWidget(self.book_description_tb)
+        self.setStyleSheet("""
+            border-style: solid;
+            border-width: 56px;
+        """)
+        self.book = book
+        self.main_layout = QtWidgets.QVBoxLayout(self)
+        self.book_cover_lb = QtWidgets.QLabel(self)
+        self.book_cover_lb.setPixmap(QtGui.QPixmap(self.book.cover_img_path))
+        self.book_title_lb = QtWidgets.QLabel(self.book.title)
+        self.book_title_lb.setStyleSheet("""
+            font-size: 18px;
+            font-weight: bold;
+        """)
+        self.main_layout.addWidget(self.book_cover_lb, 0)
+        self.main_layout.addWidget(self.book_title_lb, 1)
+
+
