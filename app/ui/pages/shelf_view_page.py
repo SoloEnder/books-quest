553bd4f412e3a2d0a2c7e187ccf0987e7252a00b
SoloEnder <houngbedjileonide@protonmail.com>
2026-02-14 22:01:38 +0100

    added page for shelf books view

---
+++ app/ui/pages/shelf_view_page.py
@@ -0,0 +1,32 @@
+
+from PySide6 import QtWidgets
+
+from app.src import book
+
+class ShelfViewPage(QtWidgets.QWidget):
+    def __init__(self, parent: None | QtWidgets.QWidget, shelf) -> None:
+        super().__init__(parent)
+        self.shelf = shelf
+
+        # Main layout
+        self.main_layout = QtWidgets.QGridLayout(self)
+        self.setLayout(self.main_layout)
+
+        self.actions_widget = QtWidgets.QWidget()
+        self.edit_b = QtWidgets.QPushButton("Modifier")
+        self.main_layout.addWidget(self.edit_b)
+        
+class ShelfActionsWidget(QtWidgets.QWidget):
+    
+    def __init__(self, parent: QtWidgets.QWidget|None, books_handler: book.BooksHandler) -> None:
+        super().__init__(parent)
+        self.books_handler = books_handler
+        
+        #Main Layout
+        self.main_layout = QtWidgets.QVBoxLayout()
+        self.setLayout(self.main_layout)
+        
+        #Widgets
+        self.edit_b = QtWidgets.QPushButton("Editer")
+        
+        
