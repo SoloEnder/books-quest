4abbf1bc00c93b285ff4ced719c0fb1536c7a2a4
SoloEnder <houngbedjileonide@protonmail.com>
2026-02-22 17:46:55 +0100

    updated code formatting for better understanding

--- app/src/my_logging_stuff.py
+++ app/src/my_logging_stuff.py
@@ -1,34 +1,48 @@
-
+import logging.handlers
 import os
 import pathlib
-import colorama
-import logging.handlers
 from tkinter.messagebox import showerror
 
-class MyRotatingFileHandler(logging.handlers.RotatingFileHandler):
 
-    def __init__(self, filename, mode='a', maxBytes=0, backupCount=0,
-                 encoding=None, delay=False, errors=None):
+class MyRotatingFileHandler(logging.handlers.RotatingFileHandler):
+    def __init__(
+        self,
+        filename,
+        mode="a",
+        maxBytes=0,
+        backupCount=0,
+        encoding=None,
+        delay=False,
+        errors=None,
+    ):
         file_parent = pathlib.Path(filename).parent
 
         if not file_parent.exists():
             file_parent.mkdir(parents=True)
 
-        super().__init__(filename, mode, maxBytes, backupCount, encoding=None, delay=False, errors=None)
+        super().__init__(
+            filename,
+            mode,
+            maxBytes,
+            backupCount,
+            encoding=None,
+            delay=False,
+            errors=None,
+        )
+
 
 class SensitiveInfoFilter(logging.Filter):
-
     def filter(self, record):
         userpath = os.path.expanduser("~")
 
         if userpath.lower() in record.getMessage().lower():
             record.msg = record.getMessage().replace(userpath, "~")
             record.args = ()
-            
-        return True
-    
-class ErrorsFilter(logging.Filter):
 
+        return True
+
+
+class ErrorsFilter(logging.Filter):
     def filter(self, record):
 
         if record.levelname == "ERROR" or record.levelname == "CRITICAL":
