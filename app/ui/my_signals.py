dc959618b774f0bd1ee8cff65e1572ca62acb3da
SoloEnder <houngbedjileonide@protonmail.com>
2026-02-22 17:47:44 +0100

    renamed my_signals.py into qt_signals_handler.py

--- app/ui/my_signals.py
+++
@@ -1,9 +0,0 @@
-from PySide6 import QtCore
-
-
-class MySignalsHandler(QtCore.QObject):
-    switch_page_signal = QtCore.Signal(str, bool, dict)
-    go_previous_page_sg = QtCore.Signal(bool)
-
-    def __init__(self):
-        super().__init__()
