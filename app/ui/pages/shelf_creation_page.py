73b55cf1de177d9e8a4b85b1a1090cb72bf4e32c
SoloEnder <houngbedjileonide@protonmail.com>
2026-02-23 22:22:56 +0100

    added shelf infos input widget[Not stable yet !]

--- app/ui/pages/shelf_creation_page.py
+++ app/ui/pages/shelf_creation_page.py
@@ -1,7 +1,10 @@
-from PySide6 import QtCore, QtWidgets
+import os
+
+from PySide6 import QtCore, QtGui, QtWidgets
 
 from app.src import book_stuff
 from app.ui import qt_signals_handler
+from app.utils import paths
 
 
 class ShelfCreationPage(QtWidgets.QWidget):
@@ -12,12 +15,79 @@ class ShelfCreationPage(QtWidgets.QWidget):
         qt_signals_handler: qt_signals_handler.QtSignalsHandler,
     ):
         super().__init__(parent)
+        self.books_handler = books_handler
+        self.qt_signals_handler = qt_signals_handler
 
-        self.main_widget = QtWidgets.QWidget()
-        self.main_lyt = QtWidgets.QGridLayout()
+        self.main_lyt = QtWidgets.QVBoxLayout()
         self.setLayout(self.main_lyt)
-        self.main_lyt.addWidget(self.main_widget)
+        self.main_widget = QtWidgets.QWidget()
+        self.main_widget_lyt = QtWidgets.QGridLayout()
+        self.main_widget.setLayout(self.main_widget_lyt)
 
+        # Back button
+        self.back_b = QtWidgets.QPushButton("Fermer")
+        self.back_b.setIcon(QtGui.QIcon(os.path.join(paths.ICONS_PATH, "back_ico.png")))
+        self.back_b.clicked.connect(
+            lambda: self.qt_signals_handler.go_previous_page_sg.emit(True)
+        )
+        self.main_lyt.addWidget(self.back_b, 0, QtCore.Qt.AlignmentFlag.AlignLeft)
+
+        # Scroll area
         self.scroll_area = QtWidgets.QScrollArea()
         self.scroll_area.setWidgetResizable(True)
-        self.scroll_area.setWidget(self)
+        self.scroll_area.setWidget(self.main_widget)
+        self.main_lyt.addWidget(self.scroll_area, 1)
+
+        # Shelf name input widget
+        self.name_lb = QtWidgets.QLabel("Nom : ")
+        self.name_e = QtWidgets.QLineEdit()
+        self.name_e.setMinimumWidth(300)
+
+        # Books selection widgets
+        self.books_tree = QtWidgets.QTreeView()
+        self.books_tree_model = QtGui.QStandardItemModel()
+        self.books_tree_model.setHorizontalHeaderLabels(("Titre", "Auteur", "Edition"))
+        self.books_tree.setModel(self.books_tree_model)
+
+        # Books items
+        for shelf in self.books_handler.books_shelfs.values():
+            shelf_item = QtGui.QStandardItem(shelf.name)
+
+            for book in shelf.books.values():
+                book_title_item = QtGui.QStandardItem(book.title)
+                book_title_item.setData(book.internal_id)
+                book_author_item = QtGui.QStandardItem(book.authors)
+                book_edition_item = QtGui.QStandardItem(book.edition)
+                shelf_item.appendRow(
+                    (book_title_item, book_author_item, book_edition_item)
+                )
+            self.books_tree_model.appendRow(shelf_item)
+
+        self.books_tree.setColumnWidth(0, 150)
+        self.books_tree.setColumnWidth(1, 150)
+        self.books_tree.setColumnWidth(2, 150)
+        self.books_tree.setMinimumWidth(460)
+        self.main_widget_lyt.addWidget(
+            self.name_lb, 0, 0, QtCore.Qt.AlignmentFlag.AlignLeft
+        )
+        self.main_widget_lyt.addWidget(
+            self.name_e, 1, 0, QtCore.Qt.AlignmentFlag.AlignLeft
+        )
+        self.main_widget_lyt.addWidget(
+            self.books_tree, 2, 0, QtCore.Qt.AlignmentFlag.AlignLeft
+        )
+
+
+class ShelfInfosInputWidget(QtWidgets.QWidget):
+    def __init__(
+        self,
+        parent: QtWidgets.QWidget | None,
+        books_handler: book_stuff.BooksHandler,
+        qt_signals_handler: qt_signals_handler.QtSignalsHandler,
+    ):
+        super().__init__(parent)
+
+        # Main layout
+
+        # Name input
+        self.name_e = QtWidgets.QLineEdit()
