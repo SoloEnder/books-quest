a13aacdc2952bc210b214237908e7ddcca6dbf5b
SoloEnder <houngbedjileonide@protonmail.com>
2026-02-27 18:05:51 +0100

    added books selection widget and shelf creation system

--- app/ui/pages/shelf_creation_page.py
+++ app/ui/pages/shelf_creation_page.py
@@ -1,3 +1,4 @@
+import datetime as dt
 import os
 
 from PySide6 import QtCore, QtGui, QtWidgets
@@ -44,29 +45,38 @@ class ShelfCreationPage(QtWidgets.QWidget):
         self.name_e.setMinimumWidth(300)
 
         # Books selection widgets
+        self.books_selection_lb = QtWidgets.QLabel("Livres : ")
         self.books_tree = QtWidgets.QTreeView()
         self.books_tree_model = QtGui.QStandardItemModel()
         self.books_tree_model.setHorizontalHeaderLabels(("Titre", "Auteur", "Edition"))
         self.books_tree.setModel(self.books_tree_model)
+        self.books_title_items = []
 
         # Books items
-        for shelf in self.books_handler.books_shelfs.values():
-            shelf_item = QtGui.QStandardItem(shelf.name)
 
-            for book in shelf.books.values():
-                book_title_item = QtGui.QStandardItem(book.title)
-                book_title_item.setData(book.internal_id)
-                book_author_item = QtGui.QStandardItem(book.authors)
-                book_edition_item = QtGui.QStandardItem(book.edition)
-                shelf_item.appendRow(
-                    (book_title_item, book_author_item, book_edition_item)
-                )
-            self.books_tree_model.appendRow(shelf_item)
+        for book in self.books_handler.books.values():
+            book_title_item = QtGui.QStandardItem(book.title)
+            book_title_item.setData(book)
+            book_title_item.setCheckable(True)
+            book_author_item = QtGui.QStandardItem(book.authors)
+            book_edition_item = QtGui.QStandardItem(book.edition)
+            self.books_tree_model.appendRow(
+                (book_title_item, book_author_item, book_edition_item)
+            )
+            self.books_title_items.append(book_title_item)
 
         self.books_tree.setColumnWidth(0, 150)
         self.books_tree.setColumnWidth(1, 150)
         self.books_tree.setColumnWidth(2, 150)
-        self.books_tree.setMinimumWidth(460)
+        self.books_tree.setEditTriggers(
+            QtWidgets.QAbstractItemView.EditTrigger.NoEditTriggers
+        )
+
+        # Confirm widgets
+        self.confirm_b = QtWidgets.QPushButton("Terminé")
+        self.confirm_b.clicked.connect(self.create_shelf)
+
+        # Add the widgets tot the layout
         self.main_widget_lyt.addWidget(
             self.name_lb, 0, 0, QtCore.Qt.AlignmentFlag.AlignLeft
         )
@@ -74,20 +84,33 @@ class ShelfCreationPage(QtWidgets.QWidget):
             self.name_e, 1, 0, QtCore.Qt.AlignmentFlag.AlignLeft
         )
         self.main_widget_lyt.addWidget(
-            self.books_tree, 2, 0, QtCore.Qt.AlignmentFlag.AlignLeft
+            self.books_selection_lb, 2, 0, QtCore.Qt.AlignmentFlag.AlignLeft
+        )
+        self.main_widget_lyt.addWidget(self.books_tree, 3, 0)
+        self.main_widget_lyt.addWidget(
+            self.confirm_b, 4, 0, QtCore.Qt.AlignmentFlag.AlignLeft
         )
 
+    def get_shelf_infos(self) -> dict | None:
+        shelf_name = self.name_e.text()
+        shelfs_books = {}
 
-class ShelfInfosInputWidget(QtWidgets.QWidget):
-    def __init__(
-        self,
-        parent: QtWidgets.QWidget | None,
-        books_handler: book_stuff.BooksHandler,
-        qt_signals_handler: qt_signals_handler.QtSignalsHandler,
-    ):
-        super().__init__(parent)
+        for book_title_item in self.books_title_items:
+            book_obj = book_title_item.data()
+            shelfs_books[book_obj.internal_id] = book_obj
 
-        # Main layout
+        if shelf_name.lower() != "tout les livres":
+            return {
+                "name": shelf_name,
+                "books": shelfs_books,
+                "id": str(dt.datetime.timestamp(dt.datetime.now())).replace(".", ""),
+            }
 
-        # Name input
-        self.name_e = QtWidgets.QLineEdit()
+        else:
+            return
+
+    def create_shelf(self):
+        shelf_infos = self.get_shelf_infos()
+
+        if shelf_infos:
+            self.books_handler.create_shelf(**shelf_infos)
