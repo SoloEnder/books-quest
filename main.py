1b75a3e2c8b1fc3df5ef8000ec7ece3092ddf95a
SoloEnder <houngbedjileonide@protonmail.com>
2026-03-11 17:45:38 +0100

    added high level errors handling

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
+import logging
+import os
+import sys
+import traceback
+
 from app import system
+from app.src import my_logging_stuff
+from app.utils import paths
+
+fmter = logging.Formatter(
+    fmt="[{asctime}] - [{name}/{levelname}] : {message}", style="{"
+)
+logger = logging.getLogger(__name__)
+mrfh = my_logging_stuff.MyRotatingFileHandler(os.path.join(paths.LOGS_PATH, "main.log"))
+mrfh.setFormatter(fmter)
+sh = logging.StreamHandler()
+sh.setFormatter(fmter)
+sh.addFilter(my_logging_stuff.ErrorsFilter())
+logger.addHandler(mrfh)
+logger.addHandler(sh)
+logger.addFilter(my_logging_stuff.SensitiveInfoFilter())
 
 if __name__ == "__main__":
-    app_system = system.AppSystem()
-    app_system.running()
+    try:
+        app_system = system.AppSystem()
+        app_system.running()
+
+    except Exception:
+        logger.critical(traceback.format_exc())
+        sys.exit()
