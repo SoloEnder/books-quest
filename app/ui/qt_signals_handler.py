482460c6ad01984c3d7c95164d67eacbe29467b8
SoloEnder <houngbedjileonide@protonmail.com>
2026-04-19 14:47:20 +0200

    fixed argument types on signals declaration

--- app/ui/qt_signals_handler.py
+++ app/ui/qt_signals_handler.py
@@ -4,7 +4,7 @@ from PySide6 import QtCore
 class QtSignalsHandler(QtCore.QObject):
     switch_page_sg = QtCore.Signal(str, bool, dict)
     go_previous_page_sg = QtCore.Signal(bool)
-    raise_error_msg_sg = QtCore.Signal(dict)
+    raise_error_msg_sg = QtCore.Signal(str)
 
     def __init__(self):
         super().__init__()
