e7a93b42cc06634aa494dd08cee1b72797b87b42
SoloEnder <houngbedjileonide@protonmail.com>
2026-04-23 17:30:52 +0200

    added method for informative notification

--- app/ui/notification_service.py
+++ app/ui/notification_service.py
@@ -6,12 +6,16 @@ class NotificationService:
     def __init__(self, widgets_parent: QtWidgets.QWidget):
         self.widgets_parent = widgets_parent
         self.default_warning_infos = {"title":"Avertissement", "msg":"Une erreur est survenue, veuillez réessayer"}
+        self.default_information_infos = {"title":"Information"}
     
     @QtCore.Slot(str, str, str, str)
     def notify(self, lvl: str|None, title: str|None, msg: str|None, details: str|None):
         
         if lvl == "error":
             self.show_error(title, msg)
+            
+        if lvl == "info":
+            self.show_info(title, msg) # type: ignore
 
     def show_error(self, title: str|None, msg: str|None):
         QtWidgets.QMessageBox.warning(
@@ -19,4 +23,12 @@ class NotificationService:
             title if title else self.default_warning_infos["title"], 
             msg if msg else self.default_warning_infos["msg"]
             )
+        
+    def show_info(self, title: str|None, msg: str):
+        QtWidgets.QMessageBox.information(
+            self.widgets_parent, 
+            title if title else self.default_information_infos["title"], 
+            msg,
+            )
+        
         
