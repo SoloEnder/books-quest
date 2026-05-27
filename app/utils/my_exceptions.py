2867a968e202fb89bae75711ff311909aa939c8a
SoloEnder <houngbedjileonide@protonmail.com>
2026-05-27 20:40:12 +0200

    improved some exceptions messages

--- app/utils/my_exceptions.py
+++ app/utils/my_exceptions.py
@@ -1,7 +1,7 @@
 class BooksShelfExistsError(Exception):
-    def __init__(self, shelf_id: str):
+    def __init__(self, shelf_id: str, msg: str|None=None):
         self.shelf_id = shelf_id
-        self.msg = f"Book shelf with the ID {shelf_id} already exists !"
+        self.msg = msg or f"Book shelf with the ID {shelf_id} already exists !"
         super().__init__(self.msg)
 
     def __str__(self):
@@ -9,9 +9,9 @@ class BooksShelfExistsError(Exception):
 
 
 class BooksShelfNotFoundError(Exception):
-    def __init__(self, shelf_id: str):
+    def __init__(self, shelf_id: str, msg: str|None=None):
         self.shelf_id = shelf_id
-        self.msg = (
+        self.msg = msg or (
             f"Book shelf with ID {shelf_id} dosen't exists ! Has been it deleted ?"
         )
         super().__init__(self.msg)
@@ -21,9 +21,9 @@ class BooksShelfNotFoundError(Exception):
 
 
 class BookNotFoundError(Exception):
-    def __init__(self, book_id: str):
+    def __init__(self, book_id: str, container, msg: str|None=None):
         self.book_id = book_id
-        self.msg = f"Book with ID {book_id} dosen't exists ! Has been it deleted ?"
+        self.msg = msg or f"Book with ID {book_id} dosen't exists in {container} ! Has been it deleted ?"
         super().__init__()
 
     def __str__(self) -> str:
@@ -31,9 +31,9 @@ class BookNotFoundError(Exception):
 
 
 class BookExistsError(Exception):
-    def __init__(self, book_id: str):
+    def __init__(self, book_id: str, container_name: str, msg: str|None=None):
         self.book_id = book_id
-        self.msg = f"Book with ID {book_id} arleady exists in this sequence !"
+        self.msg = msg or f"Book with ID {book_id} arleady exists in {container_name} !"
         super().__init__(self.msg)
 
     def __str__(self) -> str:
@@ -41,9 +41,9 @@ class BookExistsError(Exception):
 
 
 class PageNotFoundError(Exception):
-    def __init__(self, page_name: str):
+    def __init__(self, page_name: str, msg: str|None=None):
         self.page_name = page_name
-        self.msg = f"Page '{page_name}' doesn't exists !"
+        self.msg = msg or f"Page '{page_name}' doesn't exists !"
         super().__init__(self.msg)
 
     def __str__(self) -> str:
@@ -51,7 +51,7 @@ class PageNotFoundError(Exception):
     
 class PageNotLoadedError(Exception):
 
-    def __init__(self, index):
+    def __init__(self, index, msg: str|None=None):
         """
         Exception usualy raised when trying to do something on a page which is not loaded yet
         
@@ -59,7 +59,7 @@ class PageNotLoadedError(Exception):
         - index: the page virtual index
         """
         self.index = index
-        self.msg = f"Page at virtual index '{index} not loaded !"
+        self.msg = msg or f"Page at virtual index '{index} not loaded !"
         super().__init__(self.msg)
 
     def __str__(self) -> str:
@@ -67,18 +67,18 @@ class PageNotLoadedError(Exception):
     
 class InvalidDictPathError(Exception):
 
-    def __init__(self, dict_path):
+    def __init__(self, dict_path, msg: str|None=None):
         self.dict_path = dict_path
-        self.msg = f"Dict path '{self.dict_path}' isn't a valid dict path !"
+        self.msg = msg or f"Dict path '{self.dict_path}' isn't a valid dict path !"
         super().__init__(self.msg)
 
     def __str__(self) -> str:
         return self.msg
     
 class RessBasePathNotFound(Exception):
-    def __init__(self, dict_path):
+    def __init__(self, dict_path, msg: str|None=None):
         self.dict_path = dict_path
-        self.msg = f"Unable to make path with dict path '{self.dict_path}' : no valid _base_ key found !"
+        self.msg = msg or f"Unable to make path with dict path '{self.dict_path}' : no valid _base_ key found !"
         super().__init__(self.msg)
 
     def __str__(self) -> str:
@@ -86,12 +86,12 @@ class RessBasePathNotFound(Exception):
     
 class InvalidWidgetIndexError(Exception):
 
-    def __init__(self, widget):
+    def __init__(self, widget, msg: str|None=None):
         """
         Exception usually raised when the index of a 'InPageWidget' instance is not valid
         """
         self.widget = widget
-        self.msg = f"Invalid index for widget '{self.widget}' : {widget.index} !"
+        self.msg = msg or f"Invalid index for widget '{self.widget}' : {widget.index} !"
         super().__init__(self.msg)
 
     def __str__(self) -> str:
