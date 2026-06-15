523be45c019d0e0461b7946723631c9a6e49960f
SoloEnder <houngbedjileonide@protonmail.com>
2026-06-15 14:36:03 +0200

    style: applied the code format of my python extension

--- app/system.py
+++ app/system.py
@@ -1,18 +1,19 @@
-
 import logging
 import os
 import pathlib
+import time
+
 from PySide6 import QtCore, QtWidgets
 
-from app.src import book_sys
-from app.src import json_dicts_paths_handler
-from app.src import settings_handler
+from app.src import (
+    book_sys,
+    langs_handler,
+    resources_handler,
+    settings_handler,
+)
 from app.ui import ui
-from app.utils import json_file_manager
-from app.utils import paths
-from app.src import langs_handler
-from app.src import resources_handler
-import time
+from app.utils import json_file_manager, paths
+
 
 class AppSystem:
     def __init__(self, qt_app: QtWidgets.QApplication):
@@ -21,7 +22,9 @@ class AppSystem:
         self.instance_locker = None
         self.logger = logging.getLogger(__name__)
         self.jfm = json_file_manager.JsonFileManager()
-        self.res_handler = resources_handler.RessourcesHandler(self.jfm, {}, paths.BASE_PATH)
+        self.res_handler = resources_handler.RessourcesHandler(
+            self.jfm, {}, paths.BASE_PATH
+        )
         self.res_handler.load_from_file(paths.RESS_INDEXES_FILEPATH)
         self.app_infos = self.load_app_infos(self.res_handler.get_res("app_infos"))
         self.app_infos["boot_count"] += 1
@@ -37,66 +40,93 @@ class AppSystem:
                 self.res_handler.get_res("data.books"),
                 self.res_handler.get_res("data.books.covers"),
                 self.res_handler.get_res("data.bookshelves"),
-                self.res_handler.get_res("data.bookshelves.covers")
+                self.res_handler.get_res("data.bookshelves.covers"),
             )
         self.logger.info("Initialising application...")
         self.books_handler = book_sys.BooksHandler(
-            jfm=self.jfm, 
-            res_handler=self.res_handler, 
-            )
+            jfm=self.jfm,
+            res_handler=self.res_handler,
+        )
         self.books_handler.load_books(self.res_handler.get_res("data.books.books"))
-        self.books_handler.edit_default_shelf(name="Tout les livres")
-        self.books_handler.load_shelves(self.res_handler.get_res("data.bookshelves.bookshelves"))
+        self.books_handler.load_shelves(
+            self.res_handler.get_res("data.bookshelves.bookshelves")
+        )
         self.qt_app.aboutToQuit.connect(self.close_app)
         self.settings_handler = settings_handler.SettingsHandler(self.jfm)
         self.load_and_apply_settings()
         self.langs_handler = langs_handler.LangsHandler(self.jfm, {})
-        self.langs_handler.load_from_file(self.res_handler.get_res(f"assets.langs.{self.settings_handler.get_value("general.language.current")}"))
+        self.langs_handler.load_from_file(
+            self.res_handler.get_res(
+                f"assets.langs.{self.settings_handler.get_value('general.language.current')}"
+            )
+        )
         self.empty_tmp_folder(self.res_handler.get_res("tmp"))
-        
+
     def load_and_apply_settings(self):
-        self.settings_handler.load_base_settings(self.res_handler.get_res("data.settings.base"))
-        
+        self.settings_handler.load_base_settings(
+            self.res_handler.get_res("data.settings.base")
+        )
+
         try:
-            self.settings_handler.load_user_settings(self.res_handler.get_res("data.settings.user"))
-            
+            self.settings_handler.load_user_settings(
+                self.res_handler.get_res("data.settings.user")
+            )
+
         except FileNotFoundError:
-            QtWidgets.QMessageBox.warning(None, "Settings Error", "Failed to load user settings : File not found !")
-            
+            QtWidgets.QMessageBox.warning(
+                None,
+                "Settings Error",
+                "Failed to load user settings : File not found !",
+            )
+
         self.settings_handler.apply_user_settings()
 
     def start(self):
         self.start_ui()
-        
+
     def start_ui(self):
         self.logger.info("Initialising GUI...")
-        self.ui = ui.UI(self.books_handler, self.res_handler, self.settings_handler, self.langs_handler)
+        self.ui = ui.UI(
+            self.books_handler,
+            self.res_handler,
+            self.settings_handler,
+            self.langs_handler,
+        )
         self.jfm.set_signals_handler(self.ui.qt_signals_handler)
         self.ui.show()
         self.boot_end_time = time.time()
-        self.logger.info(f"Initialised app in {self.boot_end_time - self.boot_start_time:.3f}s")
-        
-        if self.app_infos["version"]["semantic"] == "indev" and self.settings_handler.get_value("developer_settings.show_indev_warning.current") == True:
+        self.logger.info(
+            f"Initialised app in {self.boot_end_time - self.boot_start_time:.3f}s"
+        )
+
+        if (
+            self.app_infos["version"]["semantic"] == "indev"
+            and self.settings_handler.get_value(
+                "developer_settings.show_indev_warning.current"
+            )
+            == True
+        ):
             self.ui.show_indev_warn()
-            
+
     def close_app(self):
         self.logger.info("Closing window...")
         self.logger.info("Saving data...")
         self.save_app_infos(self.res_handler.get_res("app_infos"))
         self.books_handler.save_books(self.res_handler.get_res("data.books.books"))
-        self.books_handler.save_shelfs(self.res_handler.get_res("data.bookshelves.bookshelves"))
+        self.books_handler.save_shelfs(
+            self.res_handler.get_res("data.bookshelves.bookshelves")
+        )
         self.logger.info("Deleting files in temporary folder...")
         self.empty_tmp_folder(self.res_handler.get_res("tmp"))
         self.settings_handler.save_settings(
             self.res_handler.get_res("data.settings.base"),
-            self.res_handler.get_res("data.settings.user")
-            )
-        self.logger.info("Exiting app...")    
-        
+            self.res_handler.get_res("data.settings.user"),
+        )
+        self.logger.info("Exiting app...")
+
     def set_instance_locker(self, instance_locker: QtCore.QLockFile):
         self.instance_locker = instance_locker
 
-            
     def empty_tmp_folder(self, dir_path):
         """
         Erase all the files in the tmp directory
@@ -123,20 +153,21 @@ class AppSystem:
             self.res_handler.get_res("data.books.covers"),
             self.res_handler.get_res("data.bookshelves"),
             self.res_handler.get_res("data.bookshelves.covers"),
-            self.res_handler.get_res("tmp")
+            self.res_handler.get_res("tmp"),
         )
         file_to_make = (
             (self.res_handler.get_res("data.books.books"), []),
             (self.res_handler.get_res("data.bookshelves.bookshelves"), []),
-            (self.res_handler.get_res("data.settings.user"), {})
+            (self.res_handler.get_res("data.settings.user"), {}),
         )
 
         for folder in folder_to_make:
-            
             if os.path.exists(folder):
-                self.logger.warning(f"Folder {folder} already exists, skipping its creation")
+                self.logger.warning(
+                    f"Folder {folder} already exists, skipping its creation"
+                )
                 continue
-                
+
             try:
                 os.mkdir(folder)
 
@@ -152,14 +183,15 @@ class AppSystem:
                 )
 
         for filepath, data in file_to_make:
-            
             if os.path.exists(filepath):
-                self.logger.warning(f"File {filepath} already exists, skipping its creation")
+                self.logger.warning(
+                    f"File {filepath} already exists, skipping its creation"
+                )
                 continue
-            
+
             if filepath.endswith(".json"):
                 self.jfm.write_json(filepath, data, catch_error=False)
-                
+
             else:
                 with open(filepath, "w") as f:
                     f.write(str(data))
