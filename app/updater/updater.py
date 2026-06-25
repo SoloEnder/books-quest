a613d9de9758fac7f86e813805b6ae75b4886b8a
SoloEnder <houngbedjileonide@protonmail.com>
2026-06-25 21:25:16 +0200

    feat: added update completion verification the updater can now check if nothing wrong has appenned during an update, an undo the changes if possible

--- app/updater/updater.py
+++ app/updater/updater.py
@@ -1,13 +1,29 @@
 import argparse
+import os
+import shutil
 
-import patch_updater
+import undo
+import utils
 
 parser = argparse.ArgumentParser()
-parser.add_argument("update_filepath", help="The path to the update file")
 parser.add_argument(
     "installation_path",
     help="The path to the folder where is located the installation to update",
 )
+parser.add_argument("-c", "--check_state", action="store_true")
+
 args = parser.parse_args()
 
-patch_updater.run(args.update_filepath, args.installation_path)
+if args.check_state:
+    update_infos = utils.read_json(
+        os.path.join(args.installation_path, "update_infos.json")
+    )
+    backup_folder = update_infos["backup_folder"]
+    update_infos_filepath = os.path.join(args.installation_path, "update_infos.json")
+    if not update_infos["state"] == "COMPLETED":
+        undo.run(
+            args.installation_path,
+            os.path.join(args.installation_path, backup_folder),
+        )
+    shutil.rmtree(backup_folder)
+    os.remove(update_infos_filepath)
