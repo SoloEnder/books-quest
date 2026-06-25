80f0f4662fc43d0884fbd3dceed4c232ec875a47
SoloEnder <houngbedjileonide@protonmail.com>
2026-06-25 21:41:25 +0200

    feat: upgrade the user installation declared version after update if an update is successfully completed, the version declared by the installation is upgraded

--- app/updater/updater.py
+++ app/updater/updater.py
@@ -15,15 +15,21 @@ parser.add_argument("-c", "--check_state", action="store_true")
 args = parser.parse_args()
 
 if args.check_state:
-    update_infos = utils.read_json(
-        os.path.join(args.installation_path, "update_infos.json")
-    )
-    backup_folder = update_infos["backup_folder"]
     update_infos_filepath = os.path.join(args.installation_path, "update_infos.json")
+    update_infos = utils.read_json(update_infos_filepath)
+    backup_folder = update_infos["backup_folder"]
     if not update_infos["state"] == "COMPLETED":
         undo.run(
             args.installation_path,
             os.path.join(args.installation_path, backup_folder),
+        )
+
+    else:
+        installation_infos = utils.get_installation_infos(args.installation_path)
+        installation_infos["version"] = update_infos["to_version"]
+        utils.write_json(
+            os.path.join(args.installation_path, "app", "app_infos.json"),
+            installation_infos,
         )
     shutil.rmtree(backup_folder)
     os.remove(update_infos_filepath)
