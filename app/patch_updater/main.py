79ff10f66149fa7db8b25abbb718307cfc638518
SoloEnder <houngbedjileonide@protonmail.com>
2026-06-26 17:00:33 +0200

    refactor: multiple changes - removed console stuff (like `print`) - added more validation on installation folder - implemented GUI

--- app/patch_updater/main.py
+++ app/patch_updater/main.py
@@ -1,34 +1,102 @@
-import argparse
 import os
+import subprocess
+import tkinter.messagebox
 import traceback
 
 import applier
+import gui
+import utils
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "installation_path",
-    help="The path to the folder where is located the installation to update",
-)
-args = parser.parse_args()
+# parser = argparse.ArgumentParser()
+# parser.add_argument(
+#     "installation_path",
+#     help="The path to the folder where is located the installation to update",
+# )
+# parser.add_argument(
+#     "--no_gui",
+#     action="store_true",
+#     help="The path to the folder where is located the installation to update",
+# )
+# args = parser.parse_args()
+# installation_path = args.installation_path
+w = gui.Window()
+w.installation_selection()
+w.resizable(False, False)
+
+
+def no_exit():
+    pass
+
+
+class InvalidInstallationError(utils.MyBaseException):
+    def __init__(self, given_path: str):
+        super().__init__()
+        self.given_path = given_path
+        self.msg = f"Element at {self.given_path} is not recognized as a valid BooksQuest installation !"
 
 
 def check_installation(installation_path: str):
     if not os.path.isdir(installation_path):
         NotADirectoryError(f"Element at {installation_path} is not a directory !")
 
+    app_folder = os.path.join(installation_path, "app")
+    app_infos = os.path.join(app_folder, "app_infos.json")
+    updater_file = os.path.join(installation_path, "updater.exe")
+    main_file = os.path.join(installation_path, "books_quest.exe")
+    excepted_content = (app_folder, app_infos, updater_file, updater_file, main_file)
 
-try:
-    print("Checking user installation...")
-    check_installation(args.installation_path)
+    for excepted in excepted_content:
+        if not os.path.exists(excepted):
+            raise InvalidInstallationError(installation_path)
 
-except NotADirectoryError:
-    print("Unable to perform update : invalid installation path !")
 
-else:
+def finish_update(updater_path, installation_path: str):
+    subprocess.Popen(
+        [
+            updater_path,
+            installation_path,
+            "-c",
+        ]
+    )
+    w.destroy()
+
+
+def try_update():
+    installation_path = w.installation_selection_fr.installation_path_sv.get()
+
     try:
-        applier.run(args.installation_path)
+        check_installation(installation_path)
 
-    except Exception:
-        print("Unable to perform update due to the following exception :")
-        print(traceback.format_exc())
-        input("Type something to exit : ")
+    except (NotADirectoryError, InvalidInstallationError):
+        tkinter.messagebox.showerror(message="Invalid installation folder !")
+
+    else:
+        updater_path = os.path.join(installation_path, "updater.exe")
+        w.update_screen()
+        w.protocol("WM_DELETE_WINDOW", func=no_exit)
+        try:
+            applier.run(installation_path)
+
+        except Exception:
+            w.protocol(
+                "WM_DELETE_WINDOW",
+                lambda: finish_update(updater_path, installation_path),
+            )
+            w.cancel_screen_fr.cancel_b.config(
+                command=lambda: finish_update(updater_path, installation_path)
+            )
+            w.cancel_screen_fr.cancel_msg_sv.set(
+                f"Couldn't perform update due to the follwing error :\n{traceback.format_exc()}"
+            )
+            w.cancel_screen()
+
+        else:
+            w.protocol(
+                "WM_DELETE_WINDOW",
+                lambda: finish_update(updater_path, installation_path),
+            )
+            w.sucess_screen()
+
+
+w.installation_selection_fr.confirm_b.config(command=try_update)
+w.mainloop()
