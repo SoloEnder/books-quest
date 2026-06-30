0ab9a16a18f315fe4c8ff12f312f6591c2a5958a
SoloEnder <houngbedjileonide@protonmail.com>
2026-06-30 18:09:16 +0200

    feat & refactor: removed unused function, added title formatter feature: added function to format books/shelves title before displaying them refactor: removed unused function

--- app/utils/utils_funcs.py
+++ app/utils/utils_funcs.py
@@ -1,56 +1,70 @@
-
 import datetime as dt
-from app.src import book_sys
-from PySide6 import QtWidgets
 import logging
+
+from PySide6 import QtWidgets
+
+from app.src import book_sys
+
 
 def unknown_book_title_fmt(book: book_sys.Book):
     creation_date = dt.datetime.fromtimestamp(float(book.id))
     return f"[Untitled]-{creation_date.date()}"
 
+
 def unknown_shelf_name_fmt(shelf: book_sys.Shelf):
     creation_date = dt.datetime.fromtimestamp(float(shelf.id))
     return f"[Unnamed]-{creation_date.date()}"
 
-def set_displayed_names(names: list|tuple) -> list:
+
+def format_displayed_title(
+    title: str,
+    format: str = "<title> (<title_suffix>)",
+    ignore_none: bool = True,
+    **kwargs,
+) -> str:
     """
-    Check the number of occurrences of the same string in <names> and format it accordingly.
-    Example:
-        names = ["one", "two", "one", "three", "three", "three"]
+    Format the displayed title by replacing the placeholders in `format` by their value given in `kwargs`
+    In `format`, each argument must be placed between '<>'.
 
-        then the result will be:
-        ["one", "two", "one (1)", "three", "three (1)", "three (2)"]
+    Parameters
+    ----------
+    title (str): the title to format
+    format (str): the excepted output (where each arg will be replaced by its value)
+    ignore_none (bool): wether to ignore format arguments that have `None` as value
+
+    Returns
+    -------
+    str: the formatted text
+
+    Example
+    -------
+    `format_dispkayer_title(title="Book", format="<title> (<occurence_count>)", occurence_count=10)`
+    will return : "Book (10)"
     """
-    displayed_names = []
-    checked_names = []
+    formatted_title = format
+    formatted_title.replace(f"<{title}>", title)
+    for key, value in kwargs.items():
+        if ignore_none and value:
+            value = ""
+        formatted_title.replace(f"<{key}>", value)
+    return formatted_title
 
-    for name in names:
-        tmp_count = 0
-        final_name = name
-        if name in checked_names:
-            for checked_name in checked_names:
-                if name == checked_name:
-                    tmp_count+=1
 
-            if tmp_count:
-                final_name = f"{name} ({tmp_count})"
-        checked_names.append(name)
-        displayed_names.append(final_name)
-
-    return displayed_names
-
-def load_and_set_ss(*filepaths, widget: QtWidgets.QWidget, logger: logging.Logger|None=None):
+def load_and_set_ss(
+    *filepaths, widget: QtWidgets.QWidget, logger: logging.Logger | None = None
+):
     combined_ss = ""
 
     for filepath in filepaths:
-        try: 
+        try:
             with open(filepath, "r") as f:
                 ss = f.read()
 
         except:
-
             if logger:
-                logger.exception(f"Couldn't load style file at {filepath}, skipping it (see logs for more infos)")
+                logger.exception(
+                    f"Couldn't load style file at {filepath}, skipping it (see logs for more infos)"
+                )
 
         else:
             combined_ss += f"\n{ss}"
