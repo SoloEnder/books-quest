cec10cee12214cf467e0582be7c7d916f1fd8ebc
SoloEnder <houngbedjileonide@protonmail.com>
2026-08-01 23:09:55 +0200

    feat: ability to edit interface theme in settings (dark, light, system)

--- app/ui/main_pages/settings_page.py
+++ app/ui/main_pages/settings_page.py
@@ -295,7 +295,78 @@ class AppearanceSettings(SettingsSection):
             self.langs_handler,
             qt_signals_handler,
         )
+        self.theme_settings = ThemeSettings(
+            None,
+            settings_handler,
+            self.langs_handler,
+            qt_signals_handler,
+        )
+        self.add_child_section(self.theme_settings)
         self.add_child_section(self.langs_settings)
+
+
+class ThemeSettings(SettingsSection):
+    """
+    The SettingsSection for the languages options
+    """
+
+    def __init__(
+        self,
+        parent: QtWidgets.QWidget | None,
+        settings_handler: settings_handler.SettingsHandler,
+        langs_handler: langs_handler.LangsHandler,
+        qt_signals_handler: qt_signals_handler.QtSignalsHandler,
+    ):
+        super().__init__(
+            parent,
+            "THEME_SETTINGS",
+            "settings.general.appearance.theme.section_title",
+            settings_handler,
+            langs_handler,
+            qt_signals_handler,
+        )
+        self.header_lb.setProperty("role", "h4")
+        self.header_lb.setStatusTip(
+            self.langs_handler.tr(
+                "settings.general.appearance.theme.section_description"
+            )
+        )
+        self.edit_theme_lb = QtWidgets.QLabel(
+            self.langs_handler.tr("settings.general.appearance.theme.interface_theme")
+        )
+        self.theme_selection_combob = QtWidgets.QComboBox()
+        self.theme_opt_indexes = {
+            "light": 0,
+            "system": 1,
+            "dark": 2,
+        }
+        self.theme_selection_combob.addItem(
+            self.langs_handler.tr("settings.general.appearance.theme.light_theme"),
+            "light",
+        )
+        self.theme_selection_combob.addItem(
+            self.langs_handler.tr(
+                "settings.general.appearance.theme.follow_system_theme"
+            ),
+            "system",
+        )
+        self.theme_selection_combob.addItem(
+            self.langs_handler.tr("settings.general.appearance.theme.dark_theme"),
+            "dark",
+        )
+        self.theme_selection_combob.setCurrentIndex(
+            self.theme_opt_indexes[
+                str(self.settings_handler.get_setting_value("general.appearance.theme"))
+            ]
+        )
+        self.base_lyt.addWidget(self.edit_theme_lb)
+        self.base_lyt.addWidget(self.theme_selection_combob, 1, 1)
+
+    def apply_settings(self):
+        self.settings_handler.set_setting_value(
+            "general.appearance.theme",
+            self.theme_selection_combob.currentData(),
+        )
 
 
 class LangsSettings(SettingsSection):
--- app/ui/ui.py
+++ app/ui/ui.py
@@ -53,6 +53,7 @@ class UI(QtWidgets.QMainWindow):
         self.langs_handler.set_current_language(
             self.settings_handler.get_setting_value("general.appearance.language")
         )
+        self.set_app_theme()
         self.books_handler.edit_default_shelf(
             title=self.langs_handler.tr("shelf.infos.default_shelf_title")
         )
@@ -93,6 +94,22 @@ class UI(QtWidgets.QMainWindow):
         )
         self.my_stacked_widgets.switch_page("SHELFS_VIEW_PAGE")
         self.setCentralWidget(self.my_stacked_widgets)
+
+    def set_app_theme(self):
+        """
+        Sets the app theme according to the settings
+        """
+        style_hints = QtWidgets.QApplication.styleHints()
+        theme = self.settings_handler.get_setting_value("general.appearance.theme")
+
+        if theme == "light":
+            style_hints.setColorScheme(QtCore.Qt.ColorScheme.Light)
+
+        elif theme == "dark":
+            style_hints.setColorScheme(QtCore.Qt.ColorScheme.Dark)
+
+        elif theme == "system":
+            style_hints.unsetColorScheme()
 
     def refresh_ui(self):
         """
