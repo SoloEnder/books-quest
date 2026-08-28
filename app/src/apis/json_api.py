9bf86cb1dacd5c5cdf1841f24012b58ed3e6c6a9
SoloEnder <houngbedjileonide@protonmail.com>
2026-08-28 18:57:51 +0200

    feat: added a API that handle JSON files operations

---
+++ app/src/apis/json_api.py
@@ -0,0 +1,34 @@
+import json
+import typing
+
+
+class JSONAPI:
+    """
+    Supports operations on JSON files
+    """
+
+    def write(self, filepath: str, data: typing.Any, encoding: str = "utf-8"):
+        """
+        Write `data` to `filepath`
+
+        Parameters
+        ----------
+        filepath (str): the file where to write the data
+        data: the data to write
+        encoding: the encoding in which to write the data
+        """
+        with open(filepath, "w", encoding=encoding) as f:
+            json.dump(data, f)
+
+    def read(self, filepath: str, encoding: str = "utf-8"):
+        """
+        Return the content of `filepath`, which must be a JSON file
+
+        Parameters
+        ----------
+        - filepath (str): the file to open
+        - encoding (str): the encoding of the file
+        """
+
+        with open(filepath, "r", encoding=encoding) as f:
+            return json.load(f)
