9c8e2acea7a70bb51d6d74822078cf5167737592
SoloEnder <houngbedjileonide@protonmail.com>
2026-09-03 10:48:12 +0200

    feat: added the BookAPI to the general API

--- app/src/api.py
+++ app/src/api.py
@@ -1,12 +1,13 @@
 import logging
 
-from app.src.apis import res_files
+from app.src.apis import books, res_files
 from app.utils import paths
 
 
 class API:
     def __init__(self):
         self.logger = logging.getLogger(f"{__name__}-GeneralAPI")
+        self.books = books.BooksAPI(self.res_files)
         self.res_files = res_files.ResourcesFilesAPI(
             paths.APP_PATH, paths.RESS_INDEXES_FILEPATH
         )
