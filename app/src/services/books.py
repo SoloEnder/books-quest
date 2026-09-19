f23af19000d1409d6c75a1dac3ce83ccbdc1495b
SoloEnder <houngbedjileonide@protonmail.com>
2026-09-19 19:33:03 +0200

    feat(BookServices): methods to add book/shelf to the books handler

--- app/src/services/books.py
+++ app/src/services/books.py
@@ -21,6 +21,20 @@ class BooksService:
         self.qt_signals = qt_signals
         self.logger = logging.getLogger(f"{__name__}-BooksService")
 
+    def add_book(self, **books_infos):
+        """
+        Create and add an instance of `Book` to the books handler
+        """
+        self.books_handler.new_book(**books_infos)
+        self.qt_signals.emit_signal("book_added_sg", books_infos["id"])
+
+    def add_shelf(self, **shelf_infos):
+        """
+        Create a Shelf object and add it to the books handler
+        """
+        self.books_handler.new_shelf(**shelf_infos)
+        self.qt_signals.emit_signal("shelf_added_sg", shelf_infos["id"])
+
     def set_cover_for(
         self,
         object_id: str,
